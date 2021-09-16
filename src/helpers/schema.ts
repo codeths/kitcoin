ba5927a587895ee3875d10225260e9036ed83f38
Albert Portnoy <albert@asportnoy.com>
2021-09-15 20:14:04 -0500

    Add user and transaction schemas

---
+++ src/helpers/schema.ts
@@ -0,0 +1,52 @@
+import * as mongoose from "mongoose";
+import { mongo as mongoURL } from "../config/keys.json";
+import { IUser, ITransaction } from "../types";
+
+mongoose.connect(mongoURL);
+
+const userSchema = new mongoose.Schema<IUser>({
+	email: {
+		type: String,
+		required: true,
+	},
+	name: {
+		type: String,
+	},
+	tokens: {
+		refresh: {
+			type: String,
+			default: null,
+		},
+		access: {
+			type: String,
+			default: null,
+		},
+		expires: {
+			type: Date,
+			default: null,
+		}
+	}
+});
+
+userSchema.index({ email: 1 }, { unique: true });
+
+const transactionSchema = new mongoose.Schema<ITransaction>({
+	amount: {
+		type: Number,
+		required: true,
+	},
+	reason: String,
+	user: String,
+	owner: String,
+	balance: {
+		type: Number,
+		required: true,
+	}
+});
+
+transactionSchema.index({ user: -1 });
+
+const User = mongoose.model<IUser>("User", userSchema);
+const Transaction = mongoose.model<ITransaction>("Transaction", transactionSchema);
+
+export { User, Transaction };
---
+++ src/types/db.ts
@@ -0,0 +1,36 @@
+/**
+ * @description A user
+ * @param {string} email The user's email
+ * @param {string} name The user's name from Google
+ * @param {tokens} tokens The user's OAuth tokens
+ */
+export interface IUser {
+	email: string,
+	name: string | null,
+	/**
+	 * @param {string} refresh OAuth refresh token
+	 * @param {string} access OAuth access token
+	 * @param {string} expires OAuth access token expiration date
+	 */
+	tokens: {
+		refresh: string | null,
+		access: string | null,
+		expires: Date | null
+	}
+}
+
+/**
+ * @description A transaction of kitcoin
+ * @param {number} amount The amount of the transaction
+ * @param {reason} [reason] The reason of the transaction
+ * @param {string} user Who the transition applies to
+ * @param {string} owner The ID of the user who performed the transaction
+ * @param {balance} balance The balance of the user after the transaction
+ */
+export interface ITransaction {
+	amount: number,
+	reason: string | null,
+	user: string,
+	owner: string,
+	balance: number,
+}
