2561472e78e27446d8790750870d3f713a6160bf
Albert Portnoy <albert@asportnoy.com>
2021-09-16 15:18:47 -0500

    Fix JSDoc types

--- src/types/db.ts
+++ src/types/db.ts
@@ -8,7 +8,7 @@ export type Document<T> = mongoose.Document<any, any, T> & T & {
  * @description A user
  * @param {string} email The user's email
  * @param {string} name The user's name from Google
- * @param {tokens} tokens The user's OAuth tokens
+ * @param {string} tokens The user's OAuth tokens
  */
 export interface IUser {
 	email: string;
@@ -16,7 +16,7 @@ export interface IUser {
 	/**
 	 * @param {string} refresh OAuth refresh token
 	 * @param {string} access OAuth access token
-	 * @param {string} expires OAuth access token expiration date
+	 * @param {Date} expires OAuth access token expiration date
 	 * @param {string} session Session token
 	 */
 	tokens: {
@@ -31,10 +31,10 @@ export interface IUser {
 /**
  * @description A transaction of kitcoin
  * @param {number} amount The amount of the transaction
- * @param {reason} [reason] The reason of the transaction
+ * @param {string} [reason] The reason of the transaction
  * @param {string} user Who the transition applies to
  * @param {string} owner The ID of the user who performed the transaction
- * @param {balance} balance The balance of the user after the transaction
+ * @param {number} balance The balance of the user after the transaction
  */
 export interface ITransaction {
 	amount: number;
