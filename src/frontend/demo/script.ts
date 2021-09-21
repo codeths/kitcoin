58c3db976860a8ed082cfaf0a7a099935a3024c2
Albert Portnoy <albert@asportnoy.com>
2021-09-21 11:28:38 -0500

    Demo

--- src/frontend/demo/script.ts
+++ src/frontend/demo/script.ts
@@ -3,8 +3,11 @@
 		name: string | null;
 		email: string | null;
 		id: string;
-	} | null = await fetch("/api/me").then(res => res && res.ok ? res.json() : null);
-	if (!me) return document.getElementById('me')!.innerHTML = "Not logged in";
+	} | null = await fetch('/api/me').then(res =>
+		res && res.ok ? res.json() : null,
+	);
+	if (!me)
+		return (document.getElementById('me')!.innerHTML = 'Not logged in');
 
 	document.getElementById('my-name')!.innerHTML = me.name || 'No Name';
 	document.getElementById('my-email')!.innerHTML = me.email || 'No Email';
