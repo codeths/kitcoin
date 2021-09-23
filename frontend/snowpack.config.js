58128487dff179114ce1c2465fa591f729bb41a7
mswider <matthew.swider@outlook.com>
2021-09-23 08:29:50 -0500

    Got Svelte + Tailwind + Snowpack working

---
+++ frontend/snowpack.config.js
@@ -0,0 +1,23 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/dist'
+  },
+  plugins: [
+    '@snowpack/plugin-svelte',
+    '@snowpack/plugin-postcss'
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    tailwindConfig: './tailwind.config.js'
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
---
+++ frontend/src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+	let name = 'world';
+</script>
+
+<h1 class="italic">Hello {name}!</h1>
