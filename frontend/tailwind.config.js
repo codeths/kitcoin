70e17f08ea447e1b90b75dcd4c1c9001076b93b3
mswider <matthew.swider@outlook.com>
2021-09-23 09:30:09 -0500

    Fix Tailwind bug

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mode: 'jit',
+  /* mode: 'jit',  this causes a problem where the css donsn't change until you reload in snowpack's dev mode */
   purge: ['./public/**/*.html', './src/**/*.{js,svelte}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
