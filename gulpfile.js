f41ab39bee55968436ae922b68c11b992bd6ec4d
Albert Portnoy <albert@albertp.dev>
2021-10-19 11:08:12 -0500

    Change build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const promisify = require('util').promisify;
+const exec = promisify(require('child_process').exec);
+const spawn = require('child_process').spawn;
+const readline = require('readline');
 
 gulp.task('default', async () => {
 	await task(typescript());
+	await frontend();
 	await task(copy());
 	return;
 });
@@ -11,8 +16,14 @@ gulp.task('copy', async () => {
 	await task(copy());
 	return;
 });
+
 gulp.task('typescript', async () => {
 	await task(typescript());
+	return;
+});
+
+gulp.task('frontend', async () => {
+	await frontend();
 	return;
 });
 
@@ -24,7 +35,7 @@ function typescript(path) {
 	const tsProject = ts.createProject('tsconfig.json');
 
 	return gulp
-		.src(path || 'src/**/*.ts', {
+		.src(path || ['src/**/*.ts', '!src/frontend/**/*'], {
 			base: 'src',
 		})
 		.pipe(tsProject())
@@ -40,16 +51,78 @@ function copy(path) {
 		.pipe(gulp.dest('dist'));
 }
 
+function frontend() {
+	return exec('npm run build --prefix src/frontend');
+}
+
 gulp.task('watch', async () => {
-	await task(typescript());
-	await task(copy());
-	return gulp.watch(['src/**/*']).on('change', async function (fileName) {
-		console.log(`${fileName} changed.`);
-		await task(
-			['ts'].some(x => fileName.endsWith(`.${x}`))
-				? typescript(fileName)
-				: copy(fileName),
-		);
-		console.log(`${fileName} copied.`);
+	node();
+	gulp.watch(['src/**/*', '!src/frontend/**/*']).on(
+		'change',
+		async function (fileName) {
+			console.log(`${fileName} changed.`);
+			await task(
+				['ts'].some(x => fileName.endsWith(`.${x}`))
+					? typescript(fileName)
+					: copy(fileName),
+			);
+			console.log(`${fileName} done.`);
+			node();
+		},
+	);
+
+	gulp.watch(['src/frontend/**/*', '!src/frontend/build/**/*']).on(
+		'change',
+		async function (fileName) {
+			console.log(`${fileName} changed.`);
+			await frontend();
+			await task(copy('src/frontend/**/*.*'));
+			console.log(`${fileName} done.`);
+			node();
+		},
+	);
+
+	console.log('Node started');
+
+	var rl = readline.createInterface({input: process.stdin});
+	rl.on('line', async line => {
+		line = line.toLowerCase();
+		if (['rs', 'restart', 'node'].includes(line)) {
+			console.log('Restarting node');
+			node();
+		} else if (['build', 'gulp'].includes(line)) {
+			console.log('Rebuilding');
+			await task(typescript());
+			await frontend();
+			await task(copy());
+			node();
+			console.log('Done');
+		} else if (['ts', 'typescript'].includes(line)) {
+			console.log('Rebuilding typescript');
+			await task(typescript());
+			node();
+			console.log('Done');
+		} else if (['copy', 'cp'].includes(line)) {
+			console.log('Copying');
+			await task(copy());
+			node();
+			console.log('Done');
+		} else if (['frontend', 'fe'].includes(line)) {
+			console.log('Rebuilding frontend');
+			await frontend();
+			await task(copy('src/frontend/**/*.*'));
+			node();
+			console.log('Done');
+		} else {
+			console.log('Unknown command.');
+		}
 	});
 });
+
+let nodeProcess = null;
+function node() {
+	if (nodeProcess) {
+		nodeProcess.kill('SIGINT');
+	}
+	nodeProcess = spawn('node', ['.']);
+}
