e244be6468e7d1615c50dd5fb5c459bcf2bb8edf
Albert Portnoy <albert@albertp.dev>
2021-10-22 18:13:25 -0500

    Body style stuff
    Co-authored-by: mswider <mswider@users.noreply.github.com

---
+++ frontend/src/utils/SetBodyStyle.svelte
@@ -0,0 +1,7 @@
+<script>
+	//This is a workaround to let us set style properties on the body object
+	export let styleString = null;
+	export let classString = null;
+	if (styleString) document.body.style = styleString;
+	if (classString) document.body.className = classString;
+</script>
