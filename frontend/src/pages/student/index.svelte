372823e56cc1c531bd84f4a206fe5853f5da6051
Albert Portnoy <albert@albertp.dev>
2021-10-22 18:13:56 -0500

    Move stuff
    Co-authored-by: mswider <mswider@users.noreply.github.com

--- frontend/src/pages/student/index.svelte
+++ frontend/src/pages/student/index.svelte
@@ -1,38 +1,9 @@
 <script>
 	import NewItems from '../../components/NewItems.svelte';
 	import Transactions from '../../components/Transactions.svelte';
-
-	async function getBalance() {
-		const res = await fetch('/api/balance/me').catch(e => null);
-		if (res && res.ok) {
-			try {
-				const json = await res.json();
-				return json.balance;
-			} catch (e) {
-				throw 'An error occured.';
-			}
-		} else {
-			throw res && res.status == 401
-				? 'Not logged in'
-				: 'An error occured.';
-		}
-	}
-
-	async function getTransactions() {
-		const res = await fetch('/api/transactions/me').catch(e => null);
-		if (res && res.ok) {
-			try {
-				const json = await res.json();
-				return json;
-			} catch (e) {
-				throw 'An error occured.';
-			}
-		} else {
-			throw res && res.status == 401
-				? 'Not logged in'
-				: 'An error occured.';
-		}
-	}
+	import Header from '../../components/Header.svelte';
+	import {getBalance, getTransactions} from '../../utils/api.js';
+	import SetBodyStyle from '../../utils/SetBodyStyle.svelte';
 
 	const newItems = [
 		{img: 'shop_images/beans.png', price: 15, name: 'Beans'},
@@ -51,24 +22,11 @@
 	];
 </script>
 
-<header>
-	<nav class="bg-blue-900 p-3 flex justify-between items-center shadow-xl">
-		<div>
-			<h2
-				class="text-white font-light text-5xl ml-2 md:ml-4 hidden sm:block"
-			>
-				Kitcoin 💵
-			</h2>
-			<h2 class="text-white font-light text-5xl ml-2 sm:hidden">💵</h2>
-			<!-- Mobile Logo -->
-		</div>
-		<div class="flex space-x-8 md:space-x-12 mr-5 md:mr-10">
-			<h3 class="text-white font-medium text-3xl border-b-4">Wallet</h3>
-			<h3 class="text-white font-medium text-3xl">Store</h3>
-		</div>
-	</nav>
-</header>
+<!-- Head  -->
+<SetBodyStyle classString="bg-gray-200" />
+<Header />
 
+<!-- Content-->
 <div class="mx-14 my-8 lg:mx-24 lg:my-14">
 	<div
 		class="space-y-10 space-x-0 lg:grid lg:grid-cols-5 lg:space-x-10 lg:space-y-0"
