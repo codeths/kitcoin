643dfbec390e1720444f056edeb97186744c9edc
Albert Portnoy <albert@albertp.dev>
2021-10-23 12:48:29 -0500

    Add store

--- src/webserver/routes/api/index.ts
+++ src/webserver/routes/api/index.ts
@@ -5,11 +5,13 @@ import currency from './currency';
 import classroom from './classroom';
 import users from './users';
 import misc from './misc';
+import store from './store';
 
 router.use(currency);
 router.use(classroom);
 router.use(users);
 router.use(misc);
+router.use(store);
 
 router.use((req, res) => res.status(404).send());
 
