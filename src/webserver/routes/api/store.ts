5d6b5649aba656e785757397a125157499177b1c
Albert Portnoy <albert@albertp.dev>
2021-10-29 18:40:14 -0500

    Add perms checking, start create item

--- src/webserver/routes/api/store.ts
+++ src/webserver/routes/api/store.ts
@@ -1,7 +1,50 @@
 import express from 'express';
+import {ClassroomClient} from '../../../helpers/classroom';
 import {request} from '../../../helpers/request';
-import {IStoreDoc, Store} from '../../../helpers/schema';
+import {IStoreDoc, IUserDoc, Store} from '../../../helpers/schema';
 const router = express.Router();
+
+async function getStorePerms(
+	store: IStoreDoc,
+	user: IUserDoc,
+): Promise<{
+	view: boolean;
+	manage: boolean;
+}> {
+	let view = false,
+		manage = false,
+		classroomClient = await new ClassroomClient().createClient(user);
+
+	if (user.hasRole('ADMIN')) {
+		manage = true;
+	} else if (store.managers.includes(user.id)) {
+		manage = true;
+	} else if (store.classID && classroomClient) {
+		const teaching = await classroomClient
+			.getClassesForRole('TEACHER')
+			.then(x => (x || []).map(x => x.id));
+
+		if (teaching.includes(store.classID)) manage = true;
+	}
+
+	if (manage)
+		return {
+			view: true,
+			manage,
+		};
+
+	if (store.public) {
+		view = true;
+	} else if (store.classID && classroomClient) {
+		const classes = await classroomClient
+			.getClassesForRole('STUDENT')
+			.then(x => (x || []).map(x => x.id));
+
+		if (classes.includes(store.classID)) view = true;
+	}
+
+	return {view, manage};
+}
 
 router.get(
 	'/store/:id',
@@ -30,21 +73,17 @@ router.get(
 
 			if (!store) return;
 
-			if (!store.public) {
-				/**
-				 * @todo Check Google classroom permissions
-				 */
-			}
+			const permissions = await getStorePerms(store, req.user);
+			if (!permissions.view) return res.status(403).send('Forbidden');
 
 			const {name, description} = store;
-			const manager = store.managers.includes(req.user.id);
 
 			const items = await store.getItems();
 
 			return res.status(200).send({
 				name,
 				description,
-				manager,
+				canManage: permissions.manage,
 				items,
 			});
 		} catch (e) {
@@ -52,6 +91,42 @@ router.get(
 				res.status(500).send('Something went wrong.');
 			} catch (e) {}
 		}
+	},
+);
+
+router.post(
+	'/storeitem',
+	async (...req) =>
+		request(...req, {
+			authentication: true,
+		}),
+	async (req, res) => {
+		if (!req.user) return;
+
+		try {
+			const {body} = req;
+			if (!body) return res.status(400).send('Bad Request');
+
+			const {storeID, name, quantity, description} = body;
+
+			const store = await Store.findById(storeID)
+				.then(store => {
+					if (!store) {
+						res.status(404).send('Store not found');
+						return null;
+					}
+					return store;
+				})
+				.catch(() => {
+					res.status(400).send('Invalid ID');
+					return null;
+				});
+
+			if (!store) return;
+
+			const permissions = await getStorePerms(store, req.user);
+			if (!permissions.manage) return res.status(403).send('Forbidden');
+		} catch (e) {}
 	},
 );
 
