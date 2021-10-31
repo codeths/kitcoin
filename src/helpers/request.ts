f5d1910dd024e131251a551641612c11537e5c30
Albert Portnoy <albert@albertp.dev>
2021-10-31 10:51:12 -0500

    Add comments and improve types

--- src/helpers/request.ts
+++ src/helpers/request.ts
@@ -42,6 +42,7 @@ export function validate(
 		notEmpty(options[x]),
 	);
 
+	// Loop through each request part (body, query, params, etc.)
 	for (let partKey of partKeys) {
 		const part = options[partKey] || {};
 		const data: {[key: string]: unknown} = req[partKey] || {};
@@ -51,12 +52,17 @@ export function validate(
 
 		const validatorKeys = Object.keys(data);
 
+		// Loop through each key of the request data
 		for (let key of validatorKeys) {
+			// Value in request data
 			const value = data[key];
+			// Validator for the key
 			const validatorOptions = part[key];
 
+			// Resolve the validator
 			const validator = validators.resolve(validatorOptions);
 
+			// Run the validator
 			const valid = validator.run(value);
 			const error = typeof valid == 'string' ? valid : null;
 			if (error)
@@ -80,14 +86,23 @@ export function validate(
 	next();
 }
 
-function numberFromData(data: unknown): number | false {
+export function stringFromData(data: string): string;
+export function stringFromData(data: unknown): string | null;
+export function stringFromData(data: unknown): string | null {
+	if (typeof data == 'string') return data;
+	return null;
+}
+
+export function numberFromData(data: number | `${number}`): number;
+export function numberFromData(data: unknown): number | null;
+export function numberFromData(data: unknown): number | null {
 	let number;
 	if (validators.numberString().run(data))
 		number = parseFloat(data as string);
 	else if (validators.number().run(data)) number = data as number;
-	else return false;
+	else return null;
 
-	if (isNaN(number)) return false;
+	if (isNaN(number)) return null;
 
 	return number;
 }
@@ -100,13 +115,18 @@ export class validators {
 		return validator;
 	};
 
+	/** Data must be a string */
 	static string = () => ({
 		run: (data: unknown): data is string => typeof data === 'string',
 		errorMessage: '{KEY} must be a string',
 	});
 
+	/**
+	 * Data must be equal to a specified value
+	 * @param value Value to compare to
+	 */
 	static streq = (value: string) => ({
-		run: (data: unknown) => {
+		run: (data: unknown): boolean | string => {
 			let isStr = this.string().run(data) as boolean;
 			if (!isStr) return '{KEY} must be a string';
 			return data == value;
@@ -114,83 +134,103 @@ export class validators {
 		errorMessage: `{KEY} must be equal to ${value}`,
 	});
 
+	/** Data must be a number */
 	static number = () => ({
 		run: (data: unknown): data is number => typeof data === 'number',
 		errorMessage: '{KEY} must be a number',
 	});
 
+	/** Data must be a stringified number */
 	static numberString = () => ({
-		run: (data: unknown) => {
+		run: (data: unknown): data is `${number}` => {
 			let isStr = this.string().run(data) as boolean;
-			if (!isStr) return '{KEY} must be a string';
+			if (!isStr) return false;
 			return !isNaN(parseFloat(data as string));
 		},
 		errorMessage: '{KEY} must be a stringified number',
 	});
 
+	/** Data must be an integer */
 	static integer = () => ({
-		run: (data: unknown) => {
-			let isNum = validators
-				.number()
-				.run(numberFromData(data)) as boolean;
-			if (!isNum) return '{KEY} must be a number';
+		run: (data: unknown): boolean | string => {
+			if (!validators.numberString().run(data))
+				return '{KEY} must be a number';
 			return (numberFromData(data) as number) % 1 === 0;
 		},
 		errorMessage: '{KEY} must be an integer',
 	});
 
+	/** Data must be a boolean */
 	static boolean = () => ({
 		run: (data: unknown): data is boolean => typeof data === 'boolean',
 		errorMessage: '{KEY} must be a boolean',
 	});
 
+	/**
+	 * Data must be greater than a specified value
+	 * @param value Value to compare to
+	 */
 	static gt = (value: number) => ({
-		run: (data: unknown) => {
-			let isNum = validators
-				.number()
-				.run(numberFromData(data)) as boolean;
-			if (!isNum) return '{KEY} must be a number';
+		run: (data: unknown): boolean | string => {
+			if (!validators.numberString().run(data))
+				return '{KEY} must be a number';
 			return numberFromData(data) > value;
 		},
 		errorMessage: `{KEY} must be greater than ${value}`,
 	});
 
+	/**
+	 * Data must be less than a specified value
+	 * @param value Value to compare to
+	 */
 	static lt = (value: number) => ({
-		run: (data: unknown) => {
-			let isNum = validators
-				.number()
-				.run(numberFromData(data)) as boolean;
-			if (!isNum) return '{KEY} must be a number';
+		run: (data: unknown): boolean | string => {
+			if (!validators.numberString().run(data))
+				return '{KEY} must be a number';
 			return numberFromData(data) < value;
 		},
 		errorMessage: `{KEY} must be less than ${value}`,
 	});
 
+	/**
+	 * Data must be equal to a specified value
+	 * @param value Value to compare to
+	 */
 	static eq = (value: number) => ({
-		run: (data: unknown) => {
-			let isNum = validators
-				.number()
-				.run(numberFromData(data)) as boolean;
-			if (!isNum) return '{KEY} must be a number';
+		run: (data: unknown): boolean | string => {
+			if (!validators.numberString().run(data))
+				return '{KEY} must be a number';
 			return numberFromData(data) === value;
 		},
 		errorMessage: `{KEY} must be equal to ${value}`,
 	});
 
+	/**
+	 * Data must be greater than or equl to a specified value
+	 * @param value Value to compare to
+	 */
 	static gte = (value: number) => ({
-		run: (data: unknown) =>
+		run: (data: unknown): boolean | string =>
 			this.gt(value).run(data) || this.eq(value).run(data),
 		errorMessage: `{KEY} must be greater than or equal to ${value}`,
 	});
 
+	/**
+	 * Data must be less than or equl to a specified value
+	 * @param value Value to compare to
+	 */
 	static lte = (value: number) => ({
-		run: (data: unknown) =>
+		run: (data: unknown): boolean | string =>
 			this.lt(value).run(data) || this.eq(value).run(data),
 		errorMessage: `{KEY} must be less than or equal to ${value}`,
 	});
 
+	/**
+	 * All validators must pass
+	 * @param validators Validators to run
+	 */
 	static and = (...validators: RequestValidateKeyOptionsResolvable[]) => ({
-		run: (data: unknown) => {
+		run: (data: unknown): boolean | string => {
 			let results: (string | boolean)[] = [];
 			validators.forEach(validator => {
 				let v = this.resolve(validator);
@@ -205,8 +245,12 @@ export class validators {
 		},
 	});
 
+	/**
+	 * At least one validator must pass
+	 * @param validators Validators to run
+	 */
 	static or = (...validators: RequestValidateKeyOptionsResolvable[]) => ({
-		run: (data: unknown) => {
+		run: (data: unknown): boolean | string => {
 			let results: (string | boolean)[] = [];
 			validators.forEach(validator => {
 				let v = this.resolve(validator);
@@ -221,8 +265,12 @@ export class validators {
 		},
 	});
 
+	/**
+	 * Validator must fail
+	 * @param validator Validator to run
+	 */
 	static not = (validator: RequestValidateKeyOptionsResolvable) => ({
-		run: (data: unknown) => {
+		run: (data: unknown): boolean | string => {
 			let v = this.resolve(validator);
 			let res = v.run(data);
 			if (res === true) return `NOT ${v.errorMessage}`;
@@ -230,13 +278,21 @@ export class validators {
 		},
 	});
 
+	/**
+	 * Vaildator must not exist (null or undefined)
+	 */
 	static exists = () => ({
-		run: (data: unknown) => data !== null && data !== undefined,
+		run: (data: unknown): data is null | undefined =>
+			data !== null && data !== undefined,
 		errorMessage: '{KEY} must exist',
 	});
 
+	/**
+	 * If data exists, validator must pass
+	 * @param validator Validator to run
+	 */
 	static optional = (validator: RequestValidateKeyOptionsResolvable) => ({
-		run: (data: unknown) => {
+		run: (data: unknown): boolean | string => {
 			let v = this.resolve(validator);
 			if (!this.exists().run(data)) return true;
 			let res = v.run(data);
