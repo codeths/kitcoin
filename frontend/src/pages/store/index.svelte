5c4b0f386d44c04569d8774d6de8c11fcb31ba62
Albert Portnoy <albert@albertp.dev>
2021-11-09 11:08:12 -0600

    Create page

---
+++ frontend/src/pages/store/index.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Header from '../../components/Header.svelte';
+	import SetBodyStyle from '../../utils/SetBodyStyle.svelte';
+</script>
+
+<!-- Head -->
+<SetBodyStyle classString="bg-gray-200" />
+<Header />
+
+<!-- Content -->
