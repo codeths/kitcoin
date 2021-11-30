b162c0d60433b8405750c6c5cf445b739180c77f
mswider <matthew.swider@outlook.com>
2021-11-30 08:31:20 -0600

    Improve new item display

--- frontend/src/components/NewItems.svelte
+++ frontend/src/components/NewItems.svelte
@@ -1,49 +1,89 @@
 <script>
+	import {onMount, onDestroy} from 'svelte';
 	/*
     Item: {img: '/path/to/image.png', price: 20, name: 'Super Cool Item'}
     This will change in the future, with things like a link being added.
   */
 	export let items = []; // An array of items. (See above)
+	let scrollElement;
 	let buttonStates = {
 		left: true,
-		right: false,
+		right: true,
 	};
 	let offset = 0;
+	let maxOffset = 0;
+	const scrollAmount = 200; //Distance to scroll when button is pressed, in pixels
+	let resizeObserver;
+	let widthInterval;
+	let overflowed = true;
 
+	const btnCheck = () => {
+		maxOffset = scrollElement.scrollWidth - scrollElement.clientWidth;
+		overflowed = scrollElement.clientWidth != scrollElement.scrollWidth;
+
+		const trueOffset = scrollElement.scrollLeft;
+		if (trueOffset < maxOffset) buttonStates.right = false;
+		if (trueOffset > 0) buttonStates.left = false;
+		if (trueOffset == 0) buttonStates.left = true;
+		if (trueOffset == maxOffset) buttonStates.right = true;
+	};
 	const handleClick = btn => {
-		// TODO: constrain this so that you can't just 'scroll' forever (50% done)
 		switch (btn) {
 			case 'left':
-				if (offset + 75 > 0) {
-					// Check if we can move
-					buttonStates.left = true;
+				if (offset - scrollAmount < 0) {
+					offset = 0;
 				} else {
-					offset += 75;
+					offset -= scrollAmount;
 				}
-				if (offset >= 0) buttonStates.left = true; // Check if we can move again
 				break;
 			case 'right':
-				offset -= 75;
-				if (offset < 0) buttonStates.left = false;
+				if (offset + scrollAmount > maxOffset) {
+					offset = maxOffset;
+				} else {
+					offset += scrollAmount;
+				}
 				break;
 		}
+		btnCheck();
+		scrollElement.scroll({left: offset, top: 0, behavior: 'smooth'});
 	};
+
+	onMount(() => {
+		if (scrollElement) {
+			maxOffset = scrollElement.scrollWidth;
+			if (window.ResizeObserver) {
+				resizeObserver = new ResizeObserver(btnCheck);
+				resizeObserver.observe(scrollElement);
+			} else {
+				widthInterval = setInterval(btnCheck, 2500);
+			}
+		}
+	});
+	onDestroy(() => {
+		if (resizeObserver) resizeObserver.unobserve(scrollElement);
+		if (widthInterval) clearInterval(widthInterval);
+	});
 </script>
 
 <div class="p-4 flex">
 	<button
-		class="p-auto px-2 flex-shrink-0"
+		class="p-auto px-2 flex-shrink-0 sm:block hidden"
 		class:opacity-40={buttonStates.left}
 		disabled={buttonStates.left}
 		on:click={() => handleClick('left')}
 	>
-		<img src="assets/arrow_left.png" alt="&larr" class="h-10 w-10" />
+		<img src="/assets/arrow_left.png" alt="&larr" class="h-10 w-10" />
 	</button>
 	<div class="flex-grow overflow-hidden">
 		{#if items.length != 0}
 			<div
-				class="flex relative transition duration-100"
-				style="transform: translate({offset}px, 0px);"
+				class="flex overflow-x-auto"
+				class:justify-evenly={!overflowed}
+				bind:this={scrollElement}
+				on:scroll={() => {
+					offset = scrollElement.scrollLeft;
+					btnCheck();
+				}}
 			>
 				{#each items as item}
 					<div
@@ -57,7 +97,9 @@
 						/>
 						<div>
 							<h2 class="text-lg text-gray-600">{item.name}</h2>
-							<h1 class="text-2xl font-bold flex items-center">
+							<h1
+								class="text-2xl font-bold flex items-center justify-start"
+							>
 								<span
 									class="icon icon-currency mr-1"
 								/>{item.price.toLocaleString()}
@@ -67,17 +109,19 @@
 				{/each}
 			</div>
 		{:else}
-			<h1>Array is empty</h1>
+			<h1 class="text-4xl text-center my-16 italic text-gray-500">
+				Nothing to show
+			</h1>
 		{/if}
 	</div>
 	<button
-		class="p-auto px-2 flex-shrink-0"
+		class="p-auto px-2 flex-shrink-0 sm:block hidden"
 		class:opacity-40={buttonStates.right}
 		disabled={buttonStates.right}
 		on:click={() => handleClick('right')}
 	>
 		<img
-			src="assets/arrow_left.png"
+			src="/assets/arrow_left.png"
 			alt="&larr"
 			class="h-10 w-10 transform rotate-180"
 		/>
