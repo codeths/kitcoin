f00beef6510d19c8acfc122e68b7af4b85796545
Albert Portnoy <albert@albertp.dev>
2021-12-02 12:06:43 -0600

    pwetty uwu

--- frontend/src/pages/_fallback.svelte
+++ frontend/src/pages/_fallback.svelte
@@ -1,13 +1,16 @@
 <script>
 	import Header from '../components/Header.svelte';
+	import Button from '../components/Button.svelte';
 	import SetBodyStyle from '../utils/SetBodyStyle.svelte';
 </script>
 
 <!-- Head -->
-<SetBodyStyle classString="bg-gray-200" />
+<SetBodyStyle classString="bg-gray-200 flex flex-col" />
 <Header />
 
 <!-- Content -->
-<div class="m-8">
-	<h1 class="text-4xl text-center">404 page not found</h1>
+<div class="m-8 flex flex-col flex-1 justify-center items-center">
+	<h1 class="text-8xl text-center">404</h1>
+	<h2 class="text-4xl text-center my-6">Page not found</h2>
+	<Button href="/home" target="_self">Home</Button>
 </div>
