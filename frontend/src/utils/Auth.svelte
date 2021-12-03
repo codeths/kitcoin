e853b892bff4306219836b639e00758bbe6f5fdc
Albert Portnoy <albert@albertp.dev>
2021-12-03 09:55:41 -0600

    Improve auth handling

--- frontend/src/utils/Auth.svelte
+++ frontend/src/utils/Auth.svelte
@@ -4,8 +4,8 @@
 	let ctx = getContext('userInfo');
 	(async () => {
 		let info = (await ctx) || null;
-		if (!info || !info.authorized) {
-			window.location.href = '/signin?hint=true';
+		if (!info) {
+			window.location.reload();
 		}
 	})();
 </script>
