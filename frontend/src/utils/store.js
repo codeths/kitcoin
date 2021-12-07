b10b65db882b10ab0744ae006374b0d53937ed6a
Albert Portnoy <albert@albertp.dev>
2021-12-06 21:36:11 -0600

    Change cache layout

--- frontend/src/utils/store.js
+++ frontend/src/utils/store.js
@@ -1,4 +1,5 @@
 import {writable} from 'svelte/store';
 
-export const storeInfo = writable([]);
-export const storeItemInfo = writable([]);
+export const storeInfo = writable(null);
+export const storeItemInfo = writable({});
+export const storeItemPages = writable({});
