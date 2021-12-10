93aaeaa81238edeabc5533c3c24f84f435c68cbd
Albert Portnoy <albert@albertp.dev>
2021-12-10 15:40:37 -0600

    Use daisy for navbar (note: mobile needs fix)

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -23,87 +23,104 @@
 		homes = homes;
 		links = links;
 	});
-
-	let dropEl;
-	let userDrop = false;
-
-	document.addEventListener('click', e => {
-		if (dropEl && dropEl !== e.target && !dropEl.contains(e.target))
-			userDrop = false;
-	});
 </script>
 
 <header class={sticky ? 'sticky top-0 z-10' : null}>
-	<div class="navbar w-full bg-blue-eths text-white p-3 shadow-xl">
-		<div class="navbar-start">
-			<a href="/" class="mx-2">
-				<h2 class="text-4xl">
-					<span class="icon-logo mr-1" />Kitcoin
-				</h2></a
-			>
-			<div class="hidden md:flex mx-2">
-				{#each links as [text, link, navigate]}
-					<a
-						class="btn btn-ghost btn-sm rounded-btn text-xl font-medium"
-						target={navigate ? '_self' : null}
-						class:border-b-4={link == current}
-						href={link}>{text}</a
+	<div class="drawer">
+		<input id="nav-drawer" type="checkbox" class="drawer-toggle" />
+		<div class="flex flex-col drawer-content">
+			<div class="navbar w-full bg-blue-eths text-white p-3 shadow-xl">
+				<div class="navbar-start">
+					<a href="/" class="mx-2">
+						<h2 class="text-4xl">
+							<span class="icon-logo mr-1" />Kitcoin
+						</h2></a
 					>
-				{/each}
-			</div>
-			{#if userInfo}
-				<div
-					class="relative w-48 max-w-full flex flex-col items-stretch"
-					bind:this={dropEl}
-				>
-					<Button
-						id="dropdownButton"
-						class="rounded-lg px-4 py-2.5 w-auto"
-						bgDarkness="700"
-						hoverDarkness="800"
-						on:click={() => (userDrop = !userDrop)}
-					>
-						{userInfo.name}
-					</Button>
-					<div
-						class="flex focus-within:flex absolute bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 w-full top-12"
-						class:hidden={!userDrop}
-					>
-						<ul
-							class="py-1 w-full"
-							aria-labelledby="dropdownButton"
-						>
-							{#each homes as [text, link]}
-								<li>
-									<a
-										tabindex="0"
-										href={link}
-										on:click={() => (userDrop = false)}
-										class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"
-										class:bg-gray-100={link == current}
-										>{text}</a
-									>
-								</li>
-							{/each}
-							<li>
-								<a
-									tabindex="0"
-									href="/signout"
-									target="_self"
-									class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"
-									>Sign Out</a
-								>
-							</li>
-						</ul>
+					<div class="hidden md:flex mx-2">
+						{#each links as [text, link, navigate]}
+							<a
+								class="btn btn-ghost btn-sm rounded-btn text-xl font-medium"
+								target={navigate ? '_self' : null}
+								class:border-b-4={link == current}
+								href={link}>{text}</a
+							>
+						{/each}
 					</div>
 				</div>
-			{:else if userInfo === null}
-				<a
-					href="/signin"
-					target="_self"
-					class="text-white font-medium text-3xl">Sign In</a
-				>
-			{/if}
+				<div class="navbar-end">
+					<div class="dropdown dropdown-end">
+						<div class="btn btn-ghost" tabindex="0">
+							<i class="fas fa-user" />
+						</div>
+						<ul
+							tabindex="0"
+							class="p-2 shadow menu dropdown-content bg-base-100 text-base-content rounded-box w-52"
+						>
+							{#if userInfo}
+								<li class="px-3 py-1">
+									{userInfo.name}
+								</li>
+								<div
+									class="divider my-0 before:bg-base-300 after:bg-base-300"
+								/>
+								{#if homes.length > 1}
+									{#each homes as [text, link]}
+										<li>
+											<a
+												class="px-3 py-1 {link ==
+												current
+													? '!cursor-not-allowed bg-base-300 hover:active:bg-base-300'
+													: ''}"
+												href={link}
+												disabled={link == current}
+												>{text}</a
+											>
+										</li>
+									{/each}
+									<div
+										class="divider my-0 before:bg-base-300 after:bg-base-300"
+									/>
+								{/if}
+								<li>
+									<a
+										class="px-3 py-1"
+										href="/signout"
+										target="_self">Sign Out</a
+									>
+								</li>
+							{:else}
+								<a
+									class="px-3 py-1"
+									href="/signin"
+									target="_self">Sign In</a
+								>
+							{/if}
+						</ul>
+					</div>
+					<div class="flex md:hidden">
+						<label
+							for="nav-drawer"
+							class="btn btn-square btn-ghost"
+						>
+							<i class="fas fa-bars" />
+						</label>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="drawer-side">
+			<label for="nav-drawer" class="drawer-overlay" />
+			<ul class="p-4 overflow-y-auto menu w-80 bg-base-100">
+				{#each links as [text, link, navigate]}
+					<li>
+						<a
+							class:bg-base-300={link == current}
+							target={navigate ? '_self' : null}
+							href={link}>{text}</a
+						>
+					</li>
+				{/each}
+			</ul>
 		</div>
 	</div>
 </header>
