76b5e3b0aeef1b61ae3e8e971b4beb79343c8431
Albert Portnoy <albert@albertp.dev>
2021-12-10 15:40:49 -0600

    Tailwind v3

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,34 +1,30 @@
 module.exports = {
-	mode: 'jit',
-	purge: {
-		content: ['index.html', './public/**/*.html', './src/**/*.{js,svelte}'],
-		safelist: [
-			'opacity-40',
-			'border-b-4',
-			'bg-gray-100',
-			'bg-blue-500',
-			'bg-blue-700',
-			'bg-red-500',
-			'bg-green-500',
-			'from-gray-300',
-			'from-blue-300',
-			'from-yellow-300',
-			'from-green-300',
-			'from-red-300',
-			'to-gray-200',
-			'to-blue-200',
-			'to-yellow-200',
-			'to-green-200',
-			'to-red-200',
-			'justify-evenly',
-			'hover:bg-blue-700',
-			'hover:bg-blue-800',
-			'hover:bg-red-700',
-			'hover:bg-green-700',
-			'disabled:bg-gray-400',
-		], // If you add a class but it never shows up in the build, add it here
-	},
-	darkMode: false, // or 'media' or 'class'
+	content: ['index.html', './public/**/*.html', './src/**/*.{js,svelte}'],
+	safelist: [
+		'opacity-40',
+		'border-b-4',
+		'bg-gray-100',
+		'bg-blue-500',
+		'bg-blue-700',
+		'bg-red-500',
+		'bg-green-500',
+		'from-gray-300',
+		'from-blue-300',
+		'from-yellow-300',
+		'from-green-300',
+		'from-red-300',
+		'to-gray-200',
+		'to-blue-200',
+		'to-yellow-200',
+		'to-green-200',
+		'to-red-200',
+		'justify-evenly',
+		'hover:bg-blue-700',
+		'hover:bg-blue-800',
+		'hover:bg-red-700',
+		'hover:bg-green-700',
+		'disabled:bg-gray-400',
+	], // If you add a class but it never shows up in the build, add it here
 	theme: {
 		extend: {
 			colors: {
@@ -37,5 +33,5 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [require('daisyui')],
 };
