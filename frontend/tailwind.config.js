990759b5f39ef4fd3b32acb86442907bfe1a920a
Albert Portnoy <albert@albertp.dev>
2021-12-10 08:37:45 -0600

    Enable jit mode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,34 +1,32 @@
 module.exports = {
-	/* mode: 'jit',  this causes a problem where the css doesn't change until you reload in snowpack's dev mode */
+	mode: 'jit',
 	purge: {
 		content: ['index.html', './public/**/*.html', './src/**/*.{js,svelte}'],
-		options: {
-			safelist: [
-				'opacity-40',
-				'border-b-4',
-				'bg-gray-100',
-				'bg-blue-500',
-				'bg-blue-700',
-				'bg-red-500',
-				'bg-green-500',
-				'from-gray-300',
-				'from-blue-300',
-				'from-yellow-300',
-				'from-green-300',
-				'from-red-300',
-				'to-gray-200',
-				'to-blue-200',
-				'to-yellow-200',
-				'to-green-200',
-				'to-red-200',
-				'justify-evenly',
-				'hover:bg-blue-700',
-				'hover:bg-blue-800',
-				'hover:bg-red-700',
-				'hover:bg-green-700',
-				'disabled:bg-gray-400',
-			], // If you add a class but it never shows up in the build, add it here
-		},
+		safelist: [
+			'opacity-40',
+			'border-b-4',
+			'bg-gray-100',
+			'bg-blue-500',
+			'bg-blue-700',
+			'bg-red-500',
+			'bg-green-500',
+			'from-gray-300',
+			'from-blue-300',
+			'from-yellow-300',
+			'from-green-300',
+			'from-red-300',
+			'to-gray-200',
+			'to-blue-200',
+			'to-yellow-200',
+			'to-green-200',
+			'to-red-200',
+			'justify-evenly',
+			'hover:bg-blue-700',
+			'hover:bg-blue-800',
+			'hover:bg-red-700',
+			'hover:bg-green-700',
+			'disabled:bg-gray-400',
+		], // If you add a class but it never shows up in the build, add it here
 	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
@@ -37,12 +35,6 @@ module.exports = {
 				'blue-eths': '#0d2240',
 				'orange-eths': '#e24301',
 			},
-		},
-	},
-	variants: {
-		extend: {
-			backgroundColor: ['disabled'],
-			cursor: ['disabled'],
 		},
 	},
 	plugins: [],
