d441195c0aaa2f5456f94c098d886343a678d058
Albert Portnoy <albert@albertp.dev>
2021-12-13 23:30:23 -0600

    Fix CSS

--- frontend/src/pages/_fallback.svelte
+++ frontend/src/pages/_fallback.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <!-- Content -->
-<div class="m-8 flex flex-col flex-1 justify-center items-center">
+<div class="m-8 flex flex-col flex-1 justify-center items-center absolute w-screen min-h-full">
 	<h1 class="text-8xl text-center">404</h1>
 	<h2 class="text-4xl text-center my-6">Page not found</h2>
 	<a class="btn btn-primary px-12" href="/" target="_self">Home</a>
