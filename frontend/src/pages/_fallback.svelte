fbce3ab362a0c9e1648f121d57ef5a4a4cc34371
Albert Portnoy <albert@albertp.dev>
2021-12-13 23:31:28 -0600

    Fix padding

--- frontend/src/pages/_fallback.svelte
+++ frontend/src/pages/_fallback.svelte
@@ -5,7 +5,9 @@
 </script>
 
 <!-- Content -->
-<div class="m-8 flex flex-col flex-1 justify-center items-center absolute w-screen min-h-full">
+<div
+	class="p-8 flex flex-col flex-1 justify-center items-center absolute w-screen min-h-full"
+>
 	<h1 class="text-8xl text-center">404</h1>
 	<h2 class="text-4xl text-center my-6">Page not found</h2>
 	<a class="btn btn-primary px-12" href="/" target="_self">Home</a>
