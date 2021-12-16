acb8d0dd11bd0da0e95c155356c3ddb3aa81a23f
Albert Portnoy <albert@albertp.dev>
2021-12-16 10:04:17 -0600

    Clear cookie

--- src/webserver/routes/auth.ts
+++ src/webserver/routes/auth.ts
@@ -52,6 +52,9 @@ function handleLogin(
 	let state = `${crypto.randomBytes(8).toString('hex')}-${encodeURIComponent(
 		redirect || '/',
 	)}`;
+	res.clearCookie('state', {
+		domain: req.hostname,
+	});
 	res.cookie('state', state, {
 		domain: req.hostname,
 	});
