88b732df918fe4dd3a9261df8c3b02b73b81bf59
Albert Portnoy <albert@albertp.dev>
2022-01-12 13:55:01 -0600

    Error API

--- src/webserver/routes/api/misc.ts
+++ src/webserver/routes/api/misc.ts
@@ -1,5 +1,41 @@
 import express from 'express';
-import {} from '../../../helpers/schema';
+import {request, Validators} from '../../../helpers/request';
+import {DBError} from '../../../helpers/schema';
 const router = express.Router();
+
+router.get(
+	'/error/:id',
+	async (req, res, next) =>
+		request(req, res, next, {
+			authentication: false,
+			validators: {
+				params: {
+					id: Validators.string,
+				},
+			},
+		}),
+	async (req, res) => {
+		try {
+			const error = await DBError.findOne({_id: req.params.id});
+			if (!error) return res.status(404).send('Error not found');
+			if (error.user && req.user?.id !== error.user)
+				return res.status(403).end();
+			return res.status(200).send(error.details || {});
+		} catch (e) {
+			try {
+				const error = await DBError.generate(
+					{
+						request: req,
+						error: e instanceof Error ? e : undefined,
+					},
+					{
+						user: req.user?.id,
+					},
+				);
+				res.status(500).send(`An error occured. Error ID: ${error.id}`);
+			} catch (e) {}
+		}
+	},
+);
 
 export default router;
