d828ac8a08344041571745e6989f17352a72fe46
Albert Portnoy <albert@albertp.dev>
2022-01-17 17:35:38 -0600

    Closes #29

--- frontend/src/components/StudentSearch.svelte
+++ frontend/src/components/StudentSearch.svelte
@@ -10,14 +10,26 @@
 	export let me = null;
 
 	let results = null;
+	let autoSubmit = false;
 
 	async function getStudents(text) {
 		if (!text) {
 			results = null;
 		} else {
+			autoSubmit = false;
+
 			results = (
 				await searchUsers(text, 15, ['STUDENT'], me).catch(e => [])
-			).map(x => ({text: x.name, value: x.id}));
+			).map(x => ({text: x.name, value: x.id, confidence: x.confidence}));
+
+			if (autoSubmit) {
+				let confidentResult = results.find(x => x.confidence == 100);
+				if (confidentResult) {
+					value = confidentResult.value;
+					query = confidentResult.text;
+					results = null;
+				}
+			}
 		}
 	}
 </script>
@@ -30,6 +42,11 @@
 	bind:error
 	on:validate
 	on:change
+	on:keydown={e => {
+		if (e.key == 'Enter') {
+			autoSubmit = true;
+		}
+	}}
 	on:search={e => getStudents(e.detail)}
 	{...$$restProps}
 />
