e6976cc6361e2a3720780c7d6852e97d0277113d
mswider <matthew.swider@outlook.com>
2022-01-25 17:29:44 -0600

    Fix scrollbar bug
    There was already a scrollbar on the side, so setting the element width to the screen width would cause another scroll bar to show up at the bottom

--- frontend/src/pages/_transition.svelte
+++ frontend/src/pages/_transition.svelte
@@ -2,6 +2,6 @@
 	import {fade} from 'svelte/transition';
 </script>
 
-<div transition:fade class="w-screen min-h-full row-start-1 col-start-1">
+<div transition:fade class="w-full min-h-full row-start-1 col-start-1">
 	<slot />
 </div>
