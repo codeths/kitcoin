df3c1c4a8ef9ee44ebbc8d71e865514608996df8
Albert Portnoy <albert@albertp.dev>
2022-02-09 11:11:34 -0600

    Add more favicons ugh browser compatability sucks

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -80,4 +80,13 @@
 
 <svelte:head>
 	<link rel="icon" href="/{favicon}.ico" />
+	<link rel="icon" type="image/png" href="/{favicon}-32.png" sizes="32x32" />
+	<link
+		rel="icon"
+		type="image/png"
+		href="/{favicon}-196.png"
+		sizes="196x196"
+	/>
+	<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+	<meta name="theme-color" content="#1a2741" />
 </svelte:head>
