bb42c4506b42e1fa41d56e299035697ae49fa38e
Albert Portnoy <albert@albertp.dev>
2022-02-10 12:25:32 -0600

    WIP

--- src/webserver/routes/api/users.ts
+++ src/webserver/routes/api/users.ts
@@ -13,36 +13,115 @@ import {getAccessToken} from '../../../helpers/oauth';
 import {FilterQuery} from 'mongoose';
 const router = express.Router();
 
-router.patch(
-	'/roles',
+interface user {
+	/**
+	 * The user's email
+	 */
+	email: string | null;
+	/**
+	 * The user's Google ID
+	 */
+	googleID: string;
+	/**
+	 * The user's school ID
+	 */
+	schoolID: string;
+	/**
+	 * The user's name
+	 */
+	name: string | null;
+	tokens: {
+		/**
+		 * OAuth refresh token
+		 */
+		refresh: string | null;
+		/**
+		 * OAuth access token
+		 */
+		access: string | null;
+		/**
+		 * OAuth access token expiration date
+		 */
+		expires: Date | null;
+		/**
+		 * Session token
+		 */
+		session: string | null;
+		/**
+		 * Authorized scopes
+		 */
+		scopes: string[];
+	};
+	/**
+	 * The user's balance
+	 */
+	balance: number;
+	/**
+	 * Staff - when their balance resets
+	 */
+	balanceExpires?: Date;
+	/**
+	 * Staff - multiplier for weekly balance
+	 */
+	weeklyBalanceMultiplier?: number;
+	/**
+	 * The user's roles (bitfield)
+	 */
+	roles: number;
+}
+
+router.post(
+	'/users',
 	async (req, res, next) =>
 		request(req, res, next, {
 			authentication: true,
 			roles: ['ADMIN'],
 			validators: {
 				body: {
-					user: Validators.objectID,
-					roles: {
-						run: (data: unknown) =>
-							typeof data == 'string' && isValidRoles(data),
-						errorMessage: 'Invalid roles list',
-					},
+					email: Validators.optional(Validators.string),
+					googleID: Validators.string,
+					schoolID: Validators.optional(Validators.string),
+					name: Validators.optional(Validators.string),
+					balance: Validators.optional(Validators.currency),
+					balanceExpires: Validators.optional(Validators.date),
+					weeklyBalanceMultiplier: Validators.optional(
+						Validators.number,
+					),
+					roles: Validators.optional(
+						Validators.array(Validators.role),
+					),
 				},
 			},
 		}),
 	async (req, res) => {
 		try {
-			if (!requestHasUser(req)) return;
+			let {body} = req;
 
-			let {user, roles} = req.body;
+			let data = {
+				email: body.email,
+				googleID: body.googleID,
+				schoolID: body.schoolID,
+				name: body.name,
+				balance: body.balance,
+				balanceExpires: body.balanceExpires,
+				weeklyBalanceMultiplier: body.weeklyBalanceMultiplier,
+				roles: body.roles || [],
+			};
 
-			const dbUser = await User.findById(user);
-			if (!dbUser) return res.status(404).send('Invalid user');
-
-			dbUser.setRoles(roles);
-			await dbUser.save();
-
-			res.status(200).send(dbUser);
+			let user = await new User(data).save().catch(async e => {
+				const error = await DBError.generate(
+					{
+						request: req,
+						error: e instanceof Error ? e : undefined,
+					},
+					{
+						user: req.user?.id,
+					},
+				);
+				res.status(500).send(`An error occured. Error ID: ${error.id}`);
+			});
+			if (!user) return;
+			res.status(200).send(user);
 		} catch (e) {
 			try {
 				const error = await DBError.generate(
@@ -54,13 +133,26 @@ router.patch(
 						user: req.user?.id,
 					},
 				);
-				res.status(500).send(
-					`Something went wrong. Error ID: ${error.id}`,
-				);
+				res.status(500).send(`An error occured. Error ID: ${error.id}`);
 			} catch (e) {}
 		}
 	},
 );
+
+/**
+ * @todo
+ */
+router.get('/users/:id');
+
+/**
+ * @todo
+ */
+router.patch('/users/:id');
+
+/**
+ * @todo
+ */
+router.delete('/users/:id');
 
 // Get my info
 router.get(
