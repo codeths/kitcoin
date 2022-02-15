192d3a7a9637bd53ca5484d70c48bb602b1c0286
Albert Portnoy <albert@albertp.dev>
2022-02-15 07:59:27 -0600

    Add z index

--- frontend/src/components/DropdownSearch.svelte
+++ frontend/src/components/DropdownSearch.svelte
@@ -173,7 +173,7 @@
 		{...$$restProps}
 	>
 		<div class="relative" slot="after-input">
-			<div class="absolute w-full mt-2">
+			<div class="absolute w-full mt-2 z-50">
 				<div
 					class="divide-y max-h-60 w-full overflow-scroll absolute border border-base-300 bg-base-100 rounded-lg {(computedResults ||
 						loading) &&
