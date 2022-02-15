e0e34ca624207bfb1ec583d157e521d41da9c3fc
Albert Portnoy <albert@albertp.dev>
2022-02-15 08:10:47 -0600

    Add header to section

--- frontend/src/pages/staff/index.svelte
+++ frontend/src/pages/staff/index.svelte
@@ -113,6 +113,7 @@
 			</div>
 		</div>
 		<div class="mx-2 my-4 col-span-12">
+			<h1 class="text-3xl font-medium mb-2">Your Students</h1>
 			<div class="bg-base-100 shadow-md rounded px-8 py-8">
 				<ClassroomSearch
 					label="Choose a class"
