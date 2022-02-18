a841e4429617e5c7487ec33beb1a8492bcde11ce
Albert Portnoy <albert@albertp.dev>
2022-02-18 17:05:18 -0600

    Start daily sync

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import {gadmin_sync_user} from './config/keys.json';
+import {AdminClient} from './helpers/admin';
+import {User} from './helpers/schema';
+
+// Start webserver
 import './webserver';
+
+// Start user sync
+if (gadmin_sync_user) {
+	User.findById(gadmin_sync_user).then(user => {
+		if (user) new AdminClient().startDailySyncs(user);
+	});
+}
