e03f4c894308ad69be63a3299eecacec49bac787
Albert Portnoy <albert@albertp.dev>
2022-02-21 16:19:01 -0600

    Add store and store item

--- src/struct/db/index.ts
+++ src/struct/db/index.ts
@@ -1,9 +1,17 @@
 import {getModelForClass, DocumentType} from '@typegoose/typegoose';
 import UserClass from './user';
 import TransactionClass from './transaction';
+import StoreClass from './store';
+import StoreItemClass from './storeitem';
 
 export const User = getModelForClass(UserClass);
 export type IUser = DocumentType<UserClass>;
 
 export const Transaction = getModelForClass(TransactionClass);
 export type ITransaction = DocumentType<TransactionClass>;
+
+export const Store = getModelForClass(StoreClass);
+export type IStore = DocumentType<StoreClass>;
+
+export const StoreItem = getModelForClass(StoreItemClass);
+export type IStoreItem = DocumentType<StoreItemClass>;
