cb25861c441bffafe36a8b9764f7457c2528b02e
Albert Portnoy <albert@albertp.dev>
2022-02-21 16:19:25 -0600

    Remove old stuff

--- src/types/db.ts
+++ src/types/db.ts
@@ -1,114 +1,8 @@
 import {Document, Model, Query, FilterQuery, LeanDocument} from 'mongoose';
 import {FuzzyQuery, Search, Callback} from 'mongoose-fuzzy-searching';
 import express from 'express';
-import {ErrorDetail, IUser, ITransaction} from '../struct';
+import {ErrorDetail, IUser, ITransaction, IStore, IStoreItem} from '../struct';
 import {Modify} from '.';
-
-export interface IStore {
-	name: string;
-	description?: string;
-	/**
-	 * Google classroom ID (if applicable)
-	 */
-	classIDs: string[];
-	/**
-	 * Should the store be shown to everyone
-	 */
-	public: boolean;
-	/**
-	 * Mongo ID of the person who owns this store
-	 */
-	owner: string;
-	/**
-	 * Mongo IDs of users who can manage this store
-	 */
-	managers: string[];
-	/**
-	 * Mongo IDs of users who can view this store
-	 */
-	users: string[];
-}
-
-export type IStoreAPIResponse = Modify<
-	IStore,
-	{
-		canManage: boolean;
-		classIDs?: string[];
-		classNames?: string[];
-		owner?: string;
-		users?: {
-			name: string;
-			id: string;
-		}[];
-		managers?: {
-			name: string;
-			id: string;
-		}[];
-	}
->;
-
-export type IStoreDoc = IStore & IStoreMethods & Document<IStore>;
-
-export type IStoreQuery = Query<IStoreDoc, IStoreDoc> & IStoreQueries;
-
-export interface IStoreMethods {
-	getItems(): Promise<IStoreItemDoc[]>;
-	toAPIResponse(canManage: boolean): Promise<IStoreAPIResponse>;
-}
-
-export interface IStoreQueries {
-	/**
-	 * @param classCode Google classroom code
-	 */
-	byClassCode(classCode: string): IStoreQuery;
-}
-
-export type IStoreModel = Model<IStoreDoc, IStoreQueries>;
-
-export interface IStoreItem {
-	/**
-	 * Which store the item is in (Mongo ID)
-	 */
-	storeID: string;
-	/**
-	 * Name of the store item
-	 */
-	name: string;
-	quantity: number | null;
-	/**
-	 * Item description
-	 */
-	description?: string;
-	price: number;
-	/**
-	 * Hash of the item's image
-	 */
-	imageHash: string | null;
-}
-
-export type IStoreItemDoc = IStoreItem &
-	IStoreItemMethods &
-	Document<IStoreItem>;
-
-export type IStoreItemQuery = Query<IStoreItemDoc, IStoreItemDoc> &
-	IStoreItemQueries;
-
-export type IStoreItemsQuery = Query<IStoreItemDoc[], IStoreItemDoc> &
-	IStoreItemQueries;
-
-export interface IStoreItemMethods {
-	getStore(): Promise<IStoreDoc | null>;
-}
-
-export interface IStoreItemQueries {
-	/**
-	 * @param storeID Store ID (Mongo ID)
-	 */
-	byStoreID(storeID: string): IStoreItemsQuery;
-}
-
-export interface IStoreItemModel
-	extends Model<IStoreItemDoc, IStoreItemQueries> {}
 
 export interface IErrorDetail {
 	/**
@@ -262,4 +156,22 @@ export type ITransactionAPIResponse = Modify<
 	}
 >;
 
-export {IUser, ITransaction};
+export type IStoreAPIResponse = Modify<
+	LeanDocument<IStore>,
+	{
+		canManage: boolean;
+		classIDs?: string[];
+		classNames?: string[];
+		owner?: string;
+		users?: {
+			name: string;
+			id: string;
+		}[];
+		managers?: {
+			name: string;
+			id: string;
+		}[];
+	}
+>;
+
+export {IUser, ITransaction, IStore, IStoreItem};
