9d4e7daa9da6bd47d860f113a0e49fc75e162533
Albert Portnoy <albert@albertp.dev>
2022-02-21 16:19:19 -0600

    Remove old stuff

--- src/helpers/schema.ts
+++ src/helpers/schema.ts
@@ -1,15 +1,6 @@
 import mongoose from 'mongoose';
 import {mongo as mongoURL, weeklyBalance} from '../config/keys.json';
 import {
-	IStoreDoc,
-	IStoreModel,
-	IStoreQueries,
-	IStoreItem,
-	IStoreItemQueries,
-	IStoreItemMethods,
-	IStoreItemsQuery,
-	IStoreItemDoc,
-	IStoreItemModel,
 	IErrorDoc,
 	IErrorModel,
 	IError,
@@ -24,89 +15,6 @@ import express from 'express';
 import {ErrorDetail, User} from '../struct';
 
 mongoose.connect(mongoURL);
-
-const storeSchema = new mongoose.Schema<IStoreDoc, IStoreModel>({
-	name: {type: String, required: true},
-	description: String,
-	classIDs: [String],
-	public: {
-		type: Boolean,
-		default: false,
-		required: true,
-	},
-	owner: String,
-	managers: [String],
-	users: [String],
-});
-
-storeSchema.query.byClassCode = function (classCode: string): IStoreQueries {
-	return this.where({classIDs: classCode});
-};
-
-storeSchema.methods.getItems = async function (): Promise<IStoreItemDoc[]> {
-	return await StoreItem.where({storeID: this.id});
-};
-
-storeSchema.methods.toAPIResponse = async function (
-	this: IStoreDoc,
-	canManage: boolean,
-): Promise<IStoreAPIResponse> {
-	let data = this.toObject({
-		getters: true,
-		versionKey: false,
-	});
-	delete data.id;
-
-	if (!canManage) {
-		let res: IStoreAPIResponse = {
-			...data,
-			users: undefined,
-			managers: undefined,
-			classIDs: undefined,
-			owner: undefined,
-			canManage,
-		};
-		return res;
-	}
-
-	let userData = await Promise.all(
-		[this.users, this.managers, this.owner].flat().map(async id => {
-			let user = await User.findById(id);
-			return user ? {name: user.name || '', id} : {name: '', id};
-		}),
-	);
-
-	let res: IStoreAPIResponse = {
-		...data,
-		users: data.users.map(id => userData.find(x => x.id == id)!),
-		managers: data.managers.map(id => userData.find(x => x.id == id)!),
-		canManage,
-	};
-
-	return res;
-};
-
-storeSchema.index({classIDs: 1});
-
-const storeItemSchema = new mongoose.Schema<IStoreItemDoc, IStoreItemModel>({
-	storeID: {type: String, required: true},
-	name: {type: String, required: true},
-	quantity: Number,
-	description: {type: String, default: ''},
-	price: Number,
-	imageHash: String,
-});
-
-storeItemSchema.query.byStoreID = function (storeID: string): IStoreItemsQuery {
-	return this.where({storeID});
-};
-
-storeItemSchema.methods.getStore =
-	async function (): Promise<IStoreDoc | null> {
-		return await Store.findById(this.storeID);
-	};
-
-storeItemSchema.index({storeID: 1});
 
 const errorSchema = new mongoose.Schema<IErrorDoc, IErrorModel>({
 	_id: {
@@ -181,13 +89,7 @@ errorSchema.statics.generate = async function (
 	return new DBError(output).save();
 };
 
-const Store = mongoose.model<IStoreDoc, IStoreModel>('Store', storeSchema);
-const StoreItem = mongoose.model<IStoreItemDoc, IStoreItemModel>(
-	'StoreItem',
-	storeItemSchema,
-);
-
 const DBError = mongoose.model<IErrorDoc, IErrorModel>('Error', errorSchema);
 
-export {Store, StoreItem, DBError};
+export {DBError};
 export * from '../types';
