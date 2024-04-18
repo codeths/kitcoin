dfa3a113776e855389f7f1ada68a812736544da1
mswider <matthew.swider@outlook.com>
2024-04-18 15:29:11 -0500

    style: improve settings page

--- frontend/src/pages/settings.svelte
+++ frontend/src/pages/settings.svelte
@@ -18,33 +18,35 @@
 
 <!-- Content-->
 <div class="mx-8 my-4">
-	<h1 class="text-3xl font-medium mb-2">Email Settings</h1>
-	{#await promise}
-		<Loading height="2rem" />
-	{:then userInfo}
-		<p class="text-xl font-medium">
-			You are currently set
-			{#if !userInfo.emails}
-				not
-			{/if}
-			to receive emails regarding Kitcoin.
-		</p>
-		<button
-			class="btn btn-primary self-end px-12 mx-1 modal-button"
-			on:click={() =>
-				toggleEmailStatus().then(() => {
-					promise = getUserInfo();
-				})}
-		>
-			Opt
-			{#if !userInfo.emails}
-				in to
-			{:else}
-				out of
-			{/if}
-			receiving emails
-		</button>
-	{:catch error}
-		{error}
-	{/await}
+	<h1 class="text-4xl font-bold mb-3">Settings</h1>
+	<div class="mx-2">
+		<h1 class="text-3xl font-medium mb-2">Emails</h1>
+		{#await promise}
+			<Loading height="2rem" />
+		{:then userInfo}
+			<p class="text-xl font-medium mb-2">
+				You are currently set
+				{#if !userInfo.emails}
+					<span class="font-bold">not</span>
+				{/if}
+				to receive emails regarding Kitcoin.
+			</p>
+			<button
+				class="btn btn-primary px-6"
+				on:click={() =>
+					toggleEmailStatus().then(() => {
+						promise = getUserInfo();
+					})}
+			>
+				Turn emails
+				{#if !userInfo.emails}
+					on
+				{:else}
+					off
+				{/if}
+			</button>
+		{:catch error}
+			{error}
+		{/await}
+	</div>
 </div>
