30d099320efa1ee0de7a6fc7a129b20653c62880
edcous <49954272+edcous@users.noreply.github.com>
2024-10-20 19:27:30 -0500

    fix: migration template

--- src/helpers/email/template.ts
+++ src/helpers/email/template.ts
@@ -13,15 +13,14 @@ export function sendTemplate(
 
 			Your Kitcoin balance (of ${amount} Kitcoin${
 			amount > 1 ? 's' : ''
-		}) has been migrated! This year, ETHS is introducing an updated version of Kitcoin. Now, you can check your balance online 24/7 by going to <a href="${hostname}">${hostname}</a>. Like before, you can spend your Kitcoin at the Wildkit Store at a ratio of 1 Kitcoin to 1 dollar.
+		}) has been migrated! This year, ETHS is introducing an updated version of Kitcoin. Now, you can <a href="${hostname}">check your balance/transactions</a> and <a href="${hostname}/store">spend Kitcoin</a> online 24/7. Like before, you can spend your Kitcoin at a ratio of 1 Kitcoin to 1 dollar. For example, a limited number of ETHS Football tickets are now available for just 3 Kitcoin each! Look for additional ways to use your Kitcoin to be announced later this year.
 			<br>
 			<br>
-			Your current balance is ${userBalance} Kitcoin${
-			userBalance > 1 ? 's' : ''
-		}. You can view your transaction history by clicking <a href="${hostname}">here</a>.
+			Your current balance is ${userBalance} Kitcoin${userBalance > 1 ? 's' : ''}.
 			<br>
 			<br>
-			Sincerely,
+			GO KITS!
+			<br>
 			<br>
 			The ETHS Kitcoin Team
 			<br>
